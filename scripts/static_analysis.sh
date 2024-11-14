8cf389bb0020b23dbb0834b6753349e2f20a7862
Dennis Grewe <dennis.grewe@hs-esslingen.de>
2024-11-14 16:06:18 +0100

    changed flake8 and black positions to run black formatting first before code analysis
    Signed-off-by: Dennis Grewe <dennis.grewe@hs-esslingen.de>

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -3,11 +3,11 @@
 # change to the root directory of the project
 cd ..
 
-# run flake8 on the taskmanager package
-flake8 taskmanager
-
 # run black on the taskmanager package
 black taskmanager
+
+# run flake8 on the taskmanager package
+flake8 taskmanager
 
 # run bandit on the taskmanager package
 bandit -r taskmanager
